63c52738966c4d3005d6a11644828378cf4c5900
swdevop <devop@shieldwolfdesigns.com>
2017-10-19 14:31:39 -0500

    added chance of rain and hid apikey

--- WeatherAppv2.py
+++ WeatherAppv2.py
@@ -4,6 +4,7 @@ from geocode import getGeocodeLocation
 
 app = Flask(__name__)
 
+darkapi = '24d67c234f99b3d0913838bdedb6148a'
 
 @app.route('/')
 def hello_world():
@@ -15,15 +16,15 @@ def weather():
     try:
         ucity = request.form['userCity']
         latitude, longitude = getGeocodeLocation(ucity)
-        # r = requests.get(
-        #    'http://api.openweathermap.org/data/2.5/weather?APPID=7fc77377fc287a3ca61556cf825e9730&lat=%s&lon=%s' % (latitude, longitude))
-        r = requests.get('https://api.darksky.net/forecast/24d67c234f99b3d0913838bdedb6148a/%s,%s' % (latitude, longitude))
+        r = requests.get('https://api.darksky.net/forecast/%s/%s,%s' % (darkapi, latitude, longitude))
         json_object = r.json()
         temp_k = json_object['currently']['temperature']
         temp_f = int(temp_k)
         city = str(ucity)
         sky = str(json_object['currently']['summary'])
-        return render_template('weather.html', temp=temp_f, city=city, sky=sky)
+        hourly = str(json_object['hourly']['summary'])
+        rainchance = int(json_object['currently']['precipProbability'])
+        return render_template('weather.html', temp=temp_f, city=city, sky=sky, hourly=hourly, rainchance=rainchance)
     except:
         return render_template('error.html')
 

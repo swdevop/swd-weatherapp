d13471f889859a7ed1cee4ae036dd84f095d625e
swdevop <devop@shieldwolfdesigns.com>
2017-10-19 01:06:24 -0500

    added error handling and changed up html pages

--- WeatherAppv2.py
+++ WeatherAppv2.py
@@ -11,14 +11,17 @@ def hello_world():
 
 @app.route('/weather', methods=['POST'])
 def weather():
-    zipcode = request.form['zip']
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?APPID=7fc77377fc287a3ca61556cf825e9730&zip=' + zipcode)
-    json_object = r.json()
-    temp_k = float(json_object['main']['temp'])
-    temp_f = int((temp_k - 273.15) * 1.8 + 32)
-    city = json_object['name']
-    sky = str(json_object['weather'][0]['description'])
-    return render_template('weather.html', temp=temp_f, city=city, sky=sky)
+    try:
+        zipcode = request.form['zip']
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?APPID=7fc77377fc287a3ca61556cf825e9730&zip=' + zipcode)
+        json_object = r.json()
+        temp_k = float(json_object['main']['temp'])
+        temp_f = int((temp_k - 273.15) * 1.8 + 32)
+        city = json_object['name']
+        sky = str(json_object['weather'][0]['description'])
+        return render_template('weather.html', temp=temp_f, city=city, sky=sky)
+    except:
+        return render_template('error.html')
 
 
 if __name__ == '__main__':
